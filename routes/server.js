0ec29dcbb83d6922bd6b9848440a985a3cf32b6d
SymbioteX <dannyxiong@mail.weber.edu>
2017-03-02 09:49:56 -0700

    Working. I hope

--- routes/server.js
+++ routes/server.js
@@ -1,16 +1,23 @@
 module.exports = function(io) {
 //connection handler
-io.on('connection', function (socket) {
-    
-    // send message
-    socket.on('send', function (data) {
-        if(!socket.username){
-            socket.emit('message', {message: 'Welcome to the chat!' + data.username})
-            socket.username = data.username;
-        } else {
-            io.sockets.emit('message', { username: socket.username, message: data.message });
-        }
+    io.on('connection', function (socket) {
+        // initialize chat variable
+        socket.on('join', function(data) {
+            if(!socket.username){
+                socket.username = data.username;
+                //socket.emit('message', {message: 'Welcome to the chat!' + socket.username});                
+            } 
+            socket.emit('message', {message: 'Welcome to the chat!' + socket.username});            
+        });    
 
+        // send message
+        socket.on('send', function (data) {
+            // if(!socket.username){
+            //     socket.emit('message', {message: 'Welcome to the chat!' + data.username})
+            //     socket.username = data.username;
+            // } else {
+                io.sockets.emit('message', { username: socket.username, message: data.message });
+            //}
+        });
     });
-});
 }
